a2bb15555cf796cfa9a0f2a5c516bbf0c13afb06
voidash <ashish.thapa477@gmail.com>
2022-01-07 05:48:09 +0545

    day 15 part 2

--- src/day_15_djikstra.rs
+++ src/day_15_djikstra.rs
@@ -2,6 +2,68 @@ use std::collections::{HashMap, VecDeque};
 use crate::utils::read_file_for_input;
 
 
+fn wrap(u: i32) -> i32 {
+    let x = u % 10;
+    if x == 0{
+        return 1;
+    }
+    x
+}
+
+pub fn part2(input: &str) -> i32 {
+    let mut map = HashMap::new();
+    let mut best = HashMap::new();
+    let mut max_x = 0;
+    let mut max_y  = 0;
+    for (y,line) in input.trim().split('\n').enumerate() {
+        for (x,c) in line.chars().enumerate() {
+            map.insert((y,x),c.to_digit(10).unwrap() as i32);
+            best.insert((y,x), i32::MAX);
+            max_x = x;
+            max_y = y;
+        }
+    }
+
+
+    let mut visit = VecDeque::new();
+    let tile_width = max_x + 1;
+    let tile_height = max_y + 1;
+    for y_tile in 0..5 {
+        for x_tile in 0..5 {
+            if x_tile == 0 && y_tile == 0 {
+                continue;
+            }
+
+            for y in 0..=max_y {
+                for x in 0..max_x {
+                    if x_tile == 0 {
+                        map.insert((y+y_tile * tile_height, x + x_tile* tile_width),wrap(map[(&(y + (y_tile -1) * tile_height, x+ x_tile * tile_width))])  );
+                    }else {
+                        map.insert((y+y_tile * tile_height, x + x_tile* tile_width),wrap(map[(&(y + (y_tile) * tile_height, x+ (x_tile-1) * tile_width))]));
+                    }
+
+                    best.insert((y + y_tile * tile_height, x + x_tile * tile_width),i32::MAX);
+                }
+            }
+        }
+    }
+    visit.push_back(((0,0),0));
+    while let Some(((y,x), cost)) = visit.pop_front() {
+        if cost < best[&(y,x)] {
+            best.insert((y,x),cost);
+            for (dy,dx) in [(1isize, 0),(-1,0),(0,1), (0,-1)] {
+                let y = (y as isize) + dy;
+                let x = (x as isize) + dx;
+                if y >= 0 && x >= 0 && y <= max_y as isize && x <= max_x as isize {
+                    visit.push_back(((y as usize ,x as usize),cost+map[&(y as usize ,x as usize)]));
+                }
+            }
+        }
+    }
+
+
+    best[&(5* (tile_height ) - 1,5 * (tile_width) -1)]
+}
 pub fn part1(input: &str) -> i32 {
     let mut map = HashMap::new();
     let mut best = HashMap::new();
@@ -29,13 +91,9 @@ pub fn part1(input: &str) -> i32 {
             }
         }
     }
-
-    best[&(max_y,max_x)]
+    best[&(max_y, max_x)]
 }
 
-pub fn part_2(input: &str) {
-    
-}
 
 #[cfg(test)]
 mod test{ 
@@ -58,7 +116,7 @@ mod test{
     }
     #[test]
     fn part_a() {
-        assert_eq!(super::part1("1163751742
+        assert_eq!(super::part2("1163751742
 1381373672
 2136511328
 3694931569
